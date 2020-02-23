442a36581efe7742c78fac75c26027f25e8b3ee9
root <1445829583@qq.com>
2020-02-24 01:08:37 +0800

    克隆第一次提交

--- git/src/com/itheima/git/FirstClass.java
+++ git/src/com/itheima/git/FirstClass.java
@@ -7,6 +7,13 @@ public class FirstClass {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
+    }
+
+    public void test() {
+        System.out.println("123");
     }
 }
